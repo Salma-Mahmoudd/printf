412981145b3c7372261cc70394800160f42a71fe
Salma-Mahmoudd <salmaaamahmouddd@gmail.com>
2023-04-24 09:34:11 -0700

    Correction of MAX and MIN number

--- 1-decimal_numbers.c
+++ 1-decimal_numbers.c
@@ -6,23 +6,21 @@
  */
 int type_int(va_list args)
 {
-	int num;
-	int place_value;
-	int count = 0;
-	int i, num_digits = 0, digit;
+	int i, digit, num;
+	int place_value = 1;
+	int num_digits = 0, count = 0, of = 0;
 
 	num = va_arg(args, int);
-
 	if (num < 0)
 	{
-		count = count +  _putchar('-');
+		count += _putchar('-');
+		if ((num + 1) * -1 == 2147483647)
+			of = 1, num += 1;
 		num = num * -1;
 	}
-	place_value = 1;
-
 	if (num <= 9)
 	{
-		count = count +  _putchar('0' + num);
+		count += _putchar('0' + num);
 		return (count);
 	}
 	i = num;
@@ -30,17 +28,20 @@ int type_int(va_list args)
 	{
 		i = i / 10;
 		num_digits++;
-	}
-	for (i = 1; i < num_digits; i++)
-	{
-		place_value = place_value * 10;
+		if (i >= 1)
+			place_value = place_value * 10;
 	}
 	for (i = 0; i < num_digits; i++)
 	{
 		digit = num / place_value;
 		num -= digit * place_value;
 		place_value /= 10;
-		count = count +  _putchar('0' + digit);
+		if (i == (num_digits - 1) && of == 1)
+		{
+			count += _putchar('0' + (digit + 1));
+			return (count);
+		}
+		count += _putchar('0' + digit);
 	}
 	return (count);
 }
--- main.c
+++ main.c
@@ -9,10 +9,29 @@
  */
 int main(void)
 {
-	_printf("%d\n", 1234);
-	printf("%d\n", 1234);
-	_printf("%i\n", 5678);
-	printf("%i\n", 5678);
+	_printf("%i\n", 1024);
+	printf("%i\n", 1024);
+
+	_printf("%i\n", -1024);
+	printf("%i\n", -1024);
+
+	_printf("%i\n", 0);
+	printf("%i\n", 0);
+
+	_printf("%i\n", INT_MAX);
+	printf("%i\n", INT_MAX);
+
+	_printf("%i\n", INT_MIN);
+	printf("%i\n", INT_MIN);
+
+	/*_printf("%d\n", INT_MAX + 1024);
+	printf("%d\n", INT_MAX + 1024);*/
+
+	_printf("There is %d bytes in %d KB\n", 1024, 1);
+	printf("There is %d bytes in %d KB\n", 1024, 1);
+
+	_printf("%i\n", -1);
+	printf("%i\n", -1);
 	_printf("%i\n", -12345);
         printf("%i\n", -12345);
 	_printf("%i\n", 9-6);
