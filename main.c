1c34e57a4b689ddc6bedf84e393249d7d91bd2b3
Reem Kamal Ghoniem <reemkamal11@gmail.com>
2023-04-25 00:03:53 -0700

    task 5

--- main.c
+++ main.c
@@ -9,11 +9,13 @@
  */
 int main(void)
 {
-        void *addr;
+	/**void *addr;
 
-        addr = (void *)0x7ffe637541f0;
+	addr = (void *)0x7ffe637541f0;
 
-        _printf("Address:[%p]\n", addr);
-    printf("Address:[%p]\n", addr);
-    return (0);
+	_printf("Address:[%p]\n", addr);
+	printf("Address:[%p]\n", addr);*/
+	
+	_printf("Address:%r\n", "addr");
+	return (0);
 }
